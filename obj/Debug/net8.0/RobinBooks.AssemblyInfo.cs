62fddbb4da6247bb36447a49d81d0a3aa54931c3
pepenauta17 <patoasesino69@gamil.com>
2024-03-22 04:46:16 -0500

    info basica

--- obj/Debug/net8.0/RobinBooks.AssemblyInfo.cs
+++ obj/Debug/net8.0/RobinBooks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RobinBooks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0da90fbe40478f4338b8a4449e7df24b81b8927d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RobinBooks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RobinBooks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
