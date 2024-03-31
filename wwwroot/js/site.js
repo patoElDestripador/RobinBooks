2947c566a1808107f7c2316c0222dfb069568c4e
pepenauta17 <patoasesino69@gamil.com>
2024-03-31 16:28:31 -0500

    se arregla front

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.getElementById("idEliminarBtn").addEventListener("click", () =>{
+    Swal.fire({
+        title: "Se ha elimnidado!",
+        text: "El libro se ha eliminado correctamente!",
+        icon: "success"
+      });
+})
+
+document.getElementById("idEditarBtn").addEventListener("click", () =>{
+    Swal.fire({
+        title: "Se ha editado!",
+        text: "El libro se ha editado correctamente!",
+        icon: "success"
+      });
+})
+
+document.getElementById("idGuardarBtn").addEventListener("click", () =>{
+    Swal.fire({
+        title: "Se ha Guardado!",
+        text: "El libro se ha Guardado correctamente!",
+        icon: "success"
+      });
+})
+
